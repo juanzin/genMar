690e3ec0fadafb0e4c0563556e3fb6fa2056acde
Juan <printf_1@hotmail.com>
2026-03-23 09:56:44 -0600

    BLUE-54 feat: no se que paso

--- genMar_database/schema/create_database.sql
+++ genMar_database/schema/create_database.sql
@@ -1,4 +1,13 @@
 
+/*
+Script: create_database.sql
+Author: Juan Onofre
+Jira: https://juanzin.atlassian.net/browse/BLUE-54
+Branch: https://github.com/juanzin/genMar/tree/FT/BLUE-54-SQL
+Date: 2026-03-22
+Purpose: create database company.
+Dependencies: none
+*/
 IF DB_ID('company') IS NULL
     create database company;
 ELSE
